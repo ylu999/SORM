047020acf9d3cb4e6c2377bd38d91ad83d23f9ae
Yushan Lu <lushan6999@gmail.com>
2016-02-11 00:49:57 -0500

    class DBConnPool

---
+++ src/com/gvace/sorm/pool/DBConnPool.java
@@ -0,0 +1,65 @@
+package com.gvace.sorm.pool;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.gvace.sorm.core.DBManager;
+
+public class DBConnPool {
+	/**
+	 * Connection pool object
+	 */
+	private List<Connection> pool;
+	/**
+	 * Min Connection Number
+	 */
+	private static final int POOL_MIN_SIZE = DBManager.getConfiguration().getPoolMinSize();
+	/**
+	 * Max Connection Number
+	 */
+	private static final int POOL_MAX_SIZE = DBManager.getConfiguration().getPoolMaxSize();
+	/**
+	 * Constructor
+	 */
+	public DBConnPool() {
+		initPool();
+	}
+	/**
+	 * Initialize connection pool, make connections count to at least POOL_MIN_SIZE
+	 */
+	public void initPool(){
+		if(pool==null){
+			pool = new ArrayList<Connection>();
+		}
+		while(pool.size()<POOL_MIN_SIZE){
+			Connection conn = DBManager.createConn();
+			if(conn==null) throw new RuntimeException("DBManager.createConn returns null");
+			pool.add(conn);
+		}
+	}
+	/**
+	 * Get a connection from connection pool
+	 * @return
+	 */
+	public synchronized Connection getConnection(){
+		int last_index = pool.size()-1;
+		if(last_index<0)initPool();
+		Connection conn = pool.get(last_index);
+		pool.remove(last_index);
+		return conn;
+	}
+	/**
+	 * Put connection back to connection pool
+	 * @param conn
+	 */
+	public synchronized void close(Connection conn){
+		if(pool.size()>=POOL_MAX_SIZE){
+			DBManager.eliminate(conn);
+		}
+		else if(pool.contains(conn)){
+			throw new RuntimeException("Pool leak!");
+		}
+		else pool.add(conn);
+	}
+}
