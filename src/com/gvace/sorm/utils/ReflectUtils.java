dd6947a97d1d76e152c153edad8316fb4fd624ef
Yushan Lu <lushan6999@gmail.com>
2016-02-11 22:43:50 -0500

    Clean some comments

--- src/com/gvace/sorm/utils/ReflectUtils.java
+++ src/com/gvace/sorm/utils/ReflectUtils.java
@@ -40,19 +40,14 @@ public class ReflectUtils {
 			setMethod = obj.getClass().getDeclaredMethod("set"+StringUtils.firstCharUpperCase(columnName),columnValue.getClass());
 			setMethod.invoke(obj, columnValue);
 		} catch (NoSuchMethodException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SecurityException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
